3b40401b092bedd17e1a83af66b1593153715c0d
Eliton Alves <elitonmeunome_2014@hotmail.com>
2022-09-05 21:03:58 -0300

    Adicionado config

--- Infra/Context/RestorantiContext.cs
+++ Infra/Context/RestorantiContext.cs
@@ -29,8 +29,8 @@ namespace Infra.Context
 
         private string GetConnectionString()
         {
-            //return "Server = 50.116.86.84; Port = 3306; Database = trans052_bancoteste; Uid = trans052_admin; Pwd = TCC@unip2022";
-            return "Server=localhost;Port=3306;Database=meubanco;Uid=root;Pwd=admin";
+            return "Server=11.102.0.21; Port = 3306; Database = restor96_restoranti; Uid = restor96_admin; Pwd = TCC@unip2022";
+            //return "Server=localhost;Port=3306;Database=meubanco;Uid=root;Pwd=admin";
         }
 
     }
